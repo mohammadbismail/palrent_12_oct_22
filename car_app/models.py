762ccd23e5317e192d849677c54af46bfa44df1a
baraa-asal <b.aboasal@gmail.com>
2022-10-16 13:10:51 +0300

    provider dashboard, , adding cars,

--- car_app/models.py
+++ car_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from user_app.models import Customer, Provider
 
+
 class Car(models.Model):
     brand = models.CharField(max_length=45)
     model = models.CharField(max_length=45)
@@ -8,32 +9,37 @@ class Car(models.Model):
     production_year = models.IntegerField()
     plate_number = models.CharField(max_length=45)
     price = models.IntegerField()
-    photo = models.FileField(blank=True)
-    provider = models.ForeignKey(Provider, related_name="cars", on_delete=models.CASCADE)
+    car_photo = models.TextField()
+    provider = models.ForeignKey(
+        Provider, related_name="cars", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
 class Booking(models.Model):
     pick_up_date = models.DateField(null=True)
     drop_off_date = models.DateField(null=True)
     status = models.CharField(max_length=45)
-    customer_book = models.ForeignKey(Customer, related_name="customers_booked", on_delete=models.CASCADE)
-    car_book = models.ForeignKey(Car, related_name="cars_booked", on_delete=models.CASCADE)
+    customer_book = models.ForeignKey(
+        Customer, related_name="customers_booked", on_delete=models.CASCADE)
+    car_book = models.ForeignKey(
+        Car, related_name="cars_booked", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
 class Customer_Feedback(models.Model):
-    rating  = models.TextField(null=True)
-    comment  = models.TextField(null=True)
-    customer_feedhback = models.ForeignKey(Customer, related_name="customers_feedback", on_delete=models.CASCADE)
+    rating = models.TextField(null=True)
+    comment = models.TextField(null=True)
+    customer_feedhback = models.ForeignKey(
+        Customer, related_name="customers_feedback", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
-
 class Car_Feedback(models.Model):
     cars = models.ManyToManyField(Car, related_name="feededbacked_cars")
-    car_feedback = models.ForeignKey(Car, related_name="cars_feedback", on_delete=models.CASCADE)
+    car_feedback = models.ForeignKey(
+        Car, related_name="cars_feedback", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
